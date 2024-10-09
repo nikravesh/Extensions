5b387a3785a2c71df0226f47fab9c3c62e7b3d3a
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2024-10-09 08:30:55 +0330

    added swagger extensions

--- src/ExtensionMethod.Endpoints/Program.cs
+++ src/ExtensionMethod.Endpoints/Program.cs
@@ -3,7 +3,7 @@ using ExtensionMethod.Endpoints.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddServices();
+builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
